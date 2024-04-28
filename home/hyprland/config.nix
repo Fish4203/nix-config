cf0b8854edafb89e327a726e1a77df7a36b09b52
BenRyc <fishs4203@gmail.com>
2024-04-28 17:21:01 +1000

    broke config

--- home/hyprland/config.nix
+++ home/hyprland/config.nix
@@ -15,7 +15,7 @@
   # See https://wiki.hyprland.org/Configuring/Keywords/ for more
 
   # Execute your favorite apps at launch
-  exec-once = "waybar & hyprpaper"; #& nm-applet 
+  # exec-once = "waybar & hyprpaper"; #& nm-applet 
   # exec-once gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
   # exec-once gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
 
@@ -25,22 +25,6 @@
     "XCURSOR_SIZE,12"
     "GDK_SCALE,1"
   ];
-
-  plugin = {
-      hyprexpo = {
-          columns = 3;
-          gap_size = 5;
-          bg_col = "rgb(111111)";
-          workspace_method = "center current"; # [center/first] [workspace] e.g. first 1 or center m+1
-
-          enable_gesture = true; # laptop touchpad, 4 fingers
-          gesture_distance = 300; # how far is the "max"
-          gesture_positive = true; # positive = swipe down. Negative = swipe up.
-      };
-      split-monitor-workspaces = {
-          count = 10;
-      };
-  };
 
   # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
   input = {
--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -16,6 +16,24 @@
 
     settings = import ./config.nix {};
 
+    extraConfig =  ''
+      plugin {
+        hyprexpo {
+            columns = 3
+            gap_size = 5
+            bg_col = rgb(111111)
+            workspace_method = center current # [center/first] [workspace] e.g. first 1 or center m+1
+
+            enable_gesture = true  # laptop touchpad, 4 fingers
+            gesture_distance = 300  # how far is the "max"
+            gesture_positive = true  # positive = swipe down. Negative = swipe up.
+        };
+        split-monitor-workspaces {
+            count = 10
+        }
+      }
+    '';
+
     plugins = [
       hyprland-plugins.packages.${pkgs.system}.hyprexpo
       split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
