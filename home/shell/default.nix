44e6fda04af640cd0316dec48e1aa08528386cd3
BenRyc <fishs4203@gmail.com>
2024-04-28 22:01:20 +1000

    bash and input

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -9,6 +9,27 @@
 
   home.file.".config/kitty/kitty.conf".source = ./kitty.conf; 
 
-  environment.etc."inputrc".source = ./inputrc;
+  # environment.etc."inputrc".source = ./inputrc;
+
+  programs.readline = {
+    enable = true;
+
+    extraConfig = ''
+      # Respect default shortcuts.
+      $include /etc/inputrc
+
+      ## arrow up
+      "\e[A":history-search-backward
+      ## arrow down
+      "\e[B":history-search-forward
+    '';
+  };
+
+  programs.bash = {
+    enable = true;
+    # bashrcExtra = ''
+    
+    # '';
+  };
   
 }
