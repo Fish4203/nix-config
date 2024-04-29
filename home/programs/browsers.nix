9b979e7bd2b78dcfc34b40abfb849738f103f14a
BenRyc <fishs4203@gmail.com>
2024-04-29 18:05:44 +1000

    fixed firefox

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -14,6 +14,16 @@
 
     firefox = {
       enable = true;
+      profiles.fish4203 = {};
     };
   };
+
+  # home.persistence."/nix/dotfiles" = {
+  #     removePrefixDirectory = true;   # for GNU Stow styled dotfile folders
+  #     allowOther = true;
+  #     directories = [
+  #       # fuse mounted from /nix/dotfiles/Firefox/.mozilla to /home/$USERNAME/.mozilla
+  #       "Firefox/.mozilla"
+  #     ];
+  #   };
 }
